ebf8bb4938dd85ce81cfc2057a4590fdb6e62521
toffiepie <144724779+toffiepie@users.noreply.github.com>
2023-10-23 14:16:51 +0300

    part 2.2

---
+++ static_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void staticMemory()
+{
+    static int a = 0;
+    std::cout << "a: " << a << std::endl;
+    a += 1;
+}
+
+int main()
+{
+    staticMemory(); // prints 0
+    staticMemory(); // prints 1
+    staticMemory(); // prints 2
+    return 0;
+}
+
+//Переменная a объявлена как static внутри функции staticMemory. 
+//Это означает, что она имеет статическую продолжительность жизни и сохраняет своё значение между вызовами функции. 
+//Это означает, что значение a будет сохранено между вызовами функции staticMemory, и оно инициализируется только 
+// один раз при первом вызове.
